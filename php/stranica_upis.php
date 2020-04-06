44242f82f39e2ff15ace084e0da58dd5d6a512c6
491415 <35456264+491415@users.noreply.github.com>
2020-04-06 17:48:28 +0200

    Code refactoring

--- php/stranica_upis.php
+++ php/stranica_upis.php
@@ -2,56 +2,55 @@
 <html>
 <head>
 
-	<title>Upis</title>
-	<link rel="shortcut icon" href="../resources/images/favicon.ico" />
-	<link rel="stylesheet" type ="text/css" href="../resources/styles/stranica_upis.css" />
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Upis</title>
+    <link rel="shortcut icon" href="../resources/images/favicon.ico" />
+    <link rel="stylesheet" type ="text/css" href="../resources/styles/stranica_upis.css" />
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 
 </head>
 <body>
-	<div align="center" id="menu">
-		<ul>
-			<li><a href="../html/pocetna.html">Početna</a></li>
-			<li><a href="../html/fakultet.html">Fakultet</a></li>
-			<li><a href="../html/privatno.html">Privatno</a></li>
-			<li><a target="_blank" href="https://www.linkedin.com/in/dino-kralj-582a04128/">Životopis</a></li>
-			<li><a href="../html/kontakt.html">Kontakt</a></li>
-		</ul>
-	</div>
+    <div align="center" id="menu">
+	<ul>
+	    <li><a href="../html/pocetna.html">Početna</a></li>
+    	    <li><a href="../html/fakultet.html">Fakultet</a></li>
+	    <li><a href="../html/privatno.html">Privatno</a></li>
+	    <li><a target="_blank" href="https://www.linkedin.com/in/dino-kralj-582a04128/">Životopis</a></li>
+	    <li><a href="../html/kontakt.html">Kontakt</a></li>
+	</ul>
+    </div>
 
-	<div id="upis">
-		<?php
+    <div id="upis">
+	<?php
 
-			$con = mysqli_connect("localhost", "root", "", "user_db");
-			//Check database connection
-			if (mysqli_connect_errno())
-			{
-				echo "Failed to connect to MySQL: " . mysqli_connect_error();
-			}
+	$con = mysqli_connect("localhost", "root", "", "user_db");
+	//Check database connection
+	if (mysqli_connect_errno())
+	{
+	    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+	}
 
-			$ime = $_POST['Ime'];
-			$prezime = $_POST['Prezime'];
-			$adresa = $_POST['Adresa'];
-			$email = $_POST['Email'];
-			$username = $_POST['Username'];
+	$ime = $_POST['Ime'];
+	$prezime = $_POST['Prezime'];
+	$adresa = $_POST['Adresa'];
+	$email = $_POST['Email'];
+	$username = $_POST['Username'];
 
-			$sql="INSERT INTO korisnici (Ime, Prezime, Adresa, Email, Username)
-			VALUES ('$ime', '$prezime', '$adresa','$email','$username')";
+	$sql="INSERT INTO korisnici (Ime, Prezime, Adresa, Email, Username)
+	VALUES ('$ime', '$prezime', '$adresa','$email','$username')";
 
-			if (!mysqli_query($con,$sql))
-			{
-				echo('Error: ' . mysqli_error($con));
-			}
+	if (!mysqli_query($con,$sql))
+	{
+	    echo('Error: ' . mysqli_error($con));
+	}
 
-			echo "Upisani ste!";
+	echo "Upisani ste!";
+	?>
+    </div>
 
-		?>
-	</div>
+    <br>
 
-	<br>
-
-	<div id="footer">
-		<a href="stranica_ispis.php">Svi korisnici</a>
-	</div>
+    <div id="footer">
+	<a href="stranica_ispis.php">Svi korisnici</a>
+    </div>
 </body>
 </html>
